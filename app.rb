0b390e27eeaecf92ce77be7ab9a3840512638df9
dchase87 <david.chase4@gmail.com>
2017-05-15 13:55:47 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Dave"
+  end
+
+  get '/hometown' do
+    "My hometown is North Bellmore, NY"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is You're My Favorite Waste of Time by Marshall Crenshaw"
+  end
 end
